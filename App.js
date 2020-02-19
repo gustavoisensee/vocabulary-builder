4ac9ad7c9a4f1a11289ced8d6fe0d55cef3ebfa9
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-02-19 17:23:52 +0100

    Testing svg

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Svg, { Circle, Rect } from 'react-native-svg';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="blue"
+          strokeWidth="2.5"
+          fill="green"
+        />
+        <Rect
+          x="15"
+          y="15"
+          width="70"
+          height="70"
+          stroke="red"
+          strokeWidth="2"
+          fill="yellow"
+        />
+      </Svg>
+      <Text style={styles.text}>
+        Dev: {JSON.stringify(__DEV__)}
+      </Text>
     </View>
   );
 }
@@ -16,4 +39,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    backgroundColor: '#000',
+    color: '#fff',
+    fontWeight: '800',
+    padding: 10
+  }
 });
