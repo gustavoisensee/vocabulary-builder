b1c8a352f755ee305ea1d02fcd2218cd19909862
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-03-14 18:14:42 +0100

    feat: create list and details of language, create basic design

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { BUTTON } from '../../../consts/colors';
+
+const Button = ({ children, onPress }) => (
+  <TouchableOpacity onPress={onPress} style={{
+    backgroundColor: BUTTON.primary,
+    padding: 16,
+    borderRadius: 25,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.3,
+    marginVertical: 16
+  }}>
+    {children}
+  </TouchableOpacity>
+);
+
+export default Button
---
+++ src/components/molecules/LanguageItem/LanguageItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const LanguageItem = ({ title }) => {
+  const navigation = useNavigation();
+  const handleRedirect = () => {
+    navigation.navigate('LanguageDetails', { item: { title } });
+  }
+
+  return (
+    <View style={{
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      marginTop: 16,
+      padding: 16,
+      border: 1,
+      borderRadius: 3,
+      shadowColor: '#000',
+      shadowOffset: { width: 2, height: 4 },
+      shadowOpacity: 0.3,
+      minHeight: 60
+    }}>
+      <TouchableOpacity onPress={handleRedirect}>
+        <Text>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default LanguageItem;
