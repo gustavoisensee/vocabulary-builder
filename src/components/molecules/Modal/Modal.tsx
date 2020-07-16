ffea7bcea530165e72dce4fd1a812422a76b3843
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-07-16 23:34:52 +0200

    fix: statusbar

--- src/components/molecules/Modal/Modal.tsx
+++ src/components/molecules/Modal/Modal.tsx
@@ -1,12 +1,5 @@
-import React, { useEffect } from 'react';
-import {
-  Alert,
-  Modal,
-  View,
-  Text,
-  TouchableHighlight,
-  StatusBar
-} from 'react-native';
+import React from 'react';
+import { Alert, Modal, View, Text, TouchableHighlight } from 'react-native';
 import styles from './styles';
 
 interface cmType {
@@ -16,32 +9,25 @@ interface cmType {
   title: string;
 }
 
-const CustomModal = ({ children, show, closeModal, title }: cmType) => {
-  useEffect(() => {
-    const barStyle = show ? 'dark-content' : 'light-content';
-    StatusBar.setBarStyle(barStyle);
-  }, [show]);
-
-  return (
-    <Modal
-      presentationStyle='pageSheet'
-      animationType='slide'
-      transparent={false}
-      visible={show}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-      }}
-    >
-      <View style={styles.container}>
-        <Text style={styles.title}>{title}</Text>
-        <TouchableHighlight onPress={() => closeModal(false)}>
-          <Text style={styles.buttonCloseTitle}>Close</Text>
-        </TouchableHighlight>
-      </View>
-      <View style={styles.content}>{children}</View>
-    </Modal>
-  );
-};
+const CustomModal = ({ children, show, closeModal, title }: cmType) => (
+  <Modal
+    presentationStyle='pageSheet'
+    animationType='slide'
+    transparent={false}
+    visible={show}
+    onRequestClose={() => {
+      Alert.alert('Modal has been closed.');
+    }}
+  >
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <TouchableHighlight onPress={() => closeModal(false)}>
+        <Text style={styles.buttonCloseTitle}>Close</Text>
+      </TouchableHighlight>
+    </View>
+    <View style={styles.content}>{children}</View>
+  </Modal>
+);
 
 CustomModal.defaultProps = {
   show: false,
